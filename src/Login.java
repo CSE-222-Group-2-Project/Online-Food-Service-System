c549b90d415862c94d9d184b20ea1d6f5c6b1684
serhhatsari <serhhatsari@gmail.com>
2022-04-28 10:48:33 +0300

    Login class and user database has been added

---
+++ src/Login.java
@@ -0,0 +1,22 @@
+public class Login {
+
+  public static void showLoginMenu() {}
+
+  public static boolean isUserExist(String username) {
+    return false;
+  }
+
+  public static boolean isPasswordValid(String password) {
+    return false;
+  }
+
+  public static void addUserToDatabase() {}
+
+  public static void deleteUserFromDatabase() {}
+
+  public static void updateUserInDatabase() {}
+
+  private static ArrayList<User> getAllUsers() {
+    return null;
+  }
+}
