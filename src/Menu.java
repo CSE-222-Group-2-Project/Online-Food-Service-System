d6ab9e16539c05d1f732f857ef24f6ebced491bb
feyzanurkucuk <92530666+feyzanurkucuk@users.noreply.github.com>
2022-04-28 19:02:06 +0300

    Add files via upload

---
+++ src/Menu.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Menu {
+    LinkedList<Food> f = new LinkedList<Food>();
+    int id; 
+
+    LinkedList<Food> get_foods(){
+        return f;
+    }
+
+    void insert_food(String name, double price){
+        Food temp= new Food(id,name,price);
+        f.add(temp);
+        id++;
+    }
+
+    void delete_food(int id){
+        f.remove(id);
+        for(int i=id+1; i<f.size(); i++){
+            f.get(i).setID(i-1);
+        }
+        id--;
+    }
+
+    
+}
