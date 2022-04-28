9ddb1b5d48610cd7fb0c91f453a89cb069fd284c
serhhatsari <serhhatsari@gmail.com>
2022-04-28 19:02:12 +0300

    BST and Driver has been added, Login class has been updated

--- src/Login.java
+++ src/Login.java
@@ -1,22 +1,69 @@
+import java.util.Scanner;
+import tree.BinarySearchTree;
+
 public class Login {
 
-  public static void showLoginMenu() {}
-
-  public static boolean isUserExist(String username) {
-    return false;
+  public static void showLoginMenu() {
+    System.out.println("ONLINE FOOD SERVICE SYSTEM\n");
+    System.out.println("\tLOGIN MENU");
+    System.out.println("1. Log In");
+    System.out.println("2. Sign Up");
+    System.out.println("3. Exit");
   }
 
-  public static boolean isPasswordValid(String password) {
-    return false;
-  }
+  public static User logIn() {
+    String username = "";
+    String password = "";
+    String correctPassword = "";
+    boolean isUserExistCheck = true;
+    boolean isPasswordValidCheck = true;
+    Scanner scanObj = new Scanner(System.in);
 
-  public static void addUserToDatabase() {}
+    while (isUserExistCheck) {
+      System.out.println("Enter your username please: ");
+      username = scanObj.next();
+      isUserExistCheck = isUserExist(username);
+      if (!isUserExistCheck) {
+        System.out.println("User does not exist! Please sign up.");
+      }
+    }
 
-  public static void deleteUserFromDatabase() {}
+    correctPassword = getUserPassword(username);
 
-  public static void updateUserInDatabase() {}
+    while (isPasswordValidCheck) {
+      System.out.println("Enter your password please");
+      password = scanObj.next();
 
-  private static ArrayList<User> getAllUsers() {
+      if (!isPasswordValid(password)) {
+        System.out.println("Password is not correct! Could not login.");
+      }
+    }
     return null;
   }
+
+  public static void signUp() {}
+
+  private static boolean isUserExist(String username) {
+    return false;
+  }
+
+  private static boolean isPasswordValid(String password) {
+    return false;
+  }
+
+  private static String getUserPassword(String username) {
+    return "deneme";
+  }
+
+  private static User getUserFromUsername(String username) {
+    return null;
+  }
+
+  private static void addUserToDatabase() {}
+
+  private static void deleteUserFromDatabase() {}
+
+  private static void updateUserInDatabase() {}
+
+  private static void getAllUsers() {}
 }
