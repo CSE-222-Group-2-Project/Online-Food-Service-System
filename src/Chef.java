1d81cc6bfdb14cb7b2cc9148383d3c5c6e902439
serhhatsari <serhhatsari@gmail.com>
2022-04-30 09:00:01 +0300

    Chef and Courier class has updated

--- src/Chef.java
+++ src/Chef.java
@@ -1,8 +1,39 @@
 package src;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Chef extends Worker {
 
-  public Chef(String _name, int _age, String _username, String _password) {
+  private int certificateNumber;
+  private Queue<Order> newOrders = new PriorityQueue<Order>();
+
+  public Chef(
+    String _name,
+    int _age,
+    String _username,
+    String _password,
+    int _certificateNumber
+  ) {
     super(_name, _age, _username, _password, "Chef");
+    this.certificateNumber = _certificateNumber;
   }
+
+  public void addOrder(Order order) {
+    newOrders.add(order);
+  }
+
+  public void cookFood() {
+    Order order = newOrders.poll();
+  }
+
+  public int getCertificateNumber() {
+    return this.certificateNumber;
+  }
+
+  public int incrementCertificateNumber() {
+    return this.certificateNumber++;
+  }
+
+  public void changeMenu() {}
 }
--- src/Courier.java
+++ src/Courier.java
@@ -1,8 +1,17 @@
 package src;
 
+import java.util.PriorityQueue;
+
 public class Courier extends Worker {
+
+  private PriorityQueue<Order> orderQueue = new PriorityQueue<>();
 
   public Courier(String _name, int _age, String _username, String _password) {
     super(_name, _age, _username, _password, "Courier");
+  }
+
+  public void deliverFood() {
+    Order order = orderQueue.poll();
+    Customer orderOwner = order.get_customer();
   }
 }
