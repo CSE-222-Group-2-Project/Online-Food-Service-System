c0f5eff56ff04c4af8fbfcb3b17af09fcef0c1f5
serhhatsari <serhhatsari@gmail.com>
2022-05-01 09:12:37 +0300

    Worker class has been updated

--- src/Worker.java
+++ src/Worker.java
@@ -3,6 +3,9 @@ package src;
 public class Worker extends User {
 
   private String job;
+  private double salary;
+  private double score;
+  private int experienceYear;
 
   public Worker(
     String _name,
@@ -14,8 +17,22 @@ public class Worker extends User {
     super(_name, _age, _username, _password);
     job = _job;
   }
-  public String getJob(){
+
+  public String getJob() {
     return job;
   }
 
+  public double getSalary() {
+    return salary;
+  }
+
+  public double getScore() {
+    return score;
+  }
+
+  public int getExperienceYear() {
+    return experienceYear;
+  }
+
+  private void calculateSalary() {}
 }
