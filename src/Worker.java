2f6cd45c03a01562e6dfe2f904b89cf846159d26
serhhatsari <serhhatsari@gmail.com>
2022-05-03 20:44:48 +0300

    Getter added for Worker class

--- src/Worker.java
+++ src/Worker.java
@@ -1,27 +1,30 @@
 package src;
 
 public class Worker extends User {
-  public enum workerStatus { // experience year/score'a göre chef statüsü değişsin. 
-  beginner, // -4
-  junior,   // 4-6
-  midLevel, // 6-8
-  senior    // +8
+
+  public enum workerStatus { // experience year/score'a göre chef statüsü değişsin.
+    beginner, // -4
+    junior, // 4-6
+    midLevel, // 6-8
+    senior, // +8
   }
+
   private String job;
   private double salary;
-  private double score = 4;  // Kovulma şartı: vote min 10 olsun. 4 altına düşünce kovulsun.
+  private double score = 4; // Kovulma şartı: vote min 10 olsun. 4 altına düşünce kovulsun.
   private int experienceYear;
   private int voteAmount = 0; // Vote kısmı atacanla konusulacak, burayi customer güncelleyecek cunku
+
   // totalScore degiskeni olusturmak yerine score*voteAmounttan bul. (aritmetik ort soruları gibi)
 
-  public Worker( 
+  public Worker(
     String _name,
     int _age,
     String _username,
     String _password,
     String _job,
     int _experienceYear
-   ){
+  ) {
     super(_name, _age, _username, _password);
     job = _job;
     experienceYear = _experienceYear;
@@ -43,10 +46,13 @@ public class Worker extends User {
     return experienceYear;
   }
 
-  public void setSalary(double newSalary){
+  public void setSalary(double newSalary) {
     salary = newSalary;
   }
 
+  public int getVoteAmount() {
+    return voteAmount;
+  }
 
   private void calculateSalary() {}
 
