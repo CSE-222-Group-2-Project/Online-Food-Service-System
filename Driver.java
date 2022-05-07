486cb217804079d27b7e979cabff955c8bb1be7f
serhhatsari <serhhatsari@gmail.com>
2022-05-07 22:16:56 +0300

    authentication updated

--- Driver.java
+++ Driver.java
@@ -3,10 +3,13 @@ import src.*;
 public class Driver {
 
   public static void main(String[] args) {
-
     Restaurant kebelekCafe = new Restaurant();
-    Admin administrator = new Admin(kebelekCafe,"Fatih Erdogan",40,"gtu1234","1234");
-    
-
+    Admin administrator = new Admin(
+      kebelekCafe,
+      "Fatih Erdogan",
+      40,
+      "gtu1234",
+      "1234"
+    );
   }
 }
