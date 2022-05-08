37020bb81b9324e85adeef63c95e2e57dae8f6d2
serhhatsari <serhhatsari@gmail.com>
2022-05-08 14:11:50 +0300

    admin updated

--- src/Admin.java
+++ src/Admin.java
@@ -55,7 +55,7 @@ public class Admin extends User {
       System.out.println("Name: " + customer.getName());
       System.out.println("Job: " + customer.getJob());
       System.out.println("Age: " + customer.getAge());
-      System.out.println("Balance: " + customer.getBalance());
+      System.out.println("Balance: " + customer.getBudget());
       System.out.println("Last Order number: " + customer.getOrderNumber());
       System.out.println("Phone Number: " + customer.getPhone());
       System.out.println("---------------");
@@ -105,9 +105,9 @@ public class Admin extends User {
 
   /**
    * Adds new worker to array list
-   * @param newWorker Accepts new worker 
+   * @param newWorker Accepts new worker
    */
-  public void hiringWorker(Worker newWorker){
+  public void hiringWorker(Worker newWorker) {
     restaurant.workers.add(newWorker);
   }
 
