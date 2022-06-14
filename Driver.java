be73dea1e9877e918168d42352a99d4080f3c82b
M.Sinan Pehlivanoglu <pehlivanoglumuhammedsinan@gmail.com>
2022-06-14 22:30:33 +0300

    demos

--- Driver.java
+++ Driver.java
@@ -2,6 +2,9 @@ import src.demo.DemoAdmin;
 import src.demo.DemoCourier;
 import src.demo.DemoCustomer;
 import src.restaurant.Restaurant;
+import src.test.TestAdmin;
+import src.test.TestChef;
+import src.test.TestCourier;
 import src.test.TestCustomer;
 
 
@@ -13,10 +16,13 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-    /*Restaurant restaurant = new Restaurant();
-    DemoAdmin.adminDemo(restaurant);
+    Restaurant restaurant = new Restaurant();
+    /*DemoAdmin.adminDemo(restaurant);
     */
-    testDemos();
+    //testDemos();
+    TestChef.testChef(restaurant);
+    //TestAdmin.testAdmin(restaurant);
+    //TestCourier.testCourier(restaurant);
   }
 
   public static void testUsers() {
--- src/test/TestCourier.java
+++ src/test/TestCourier.java
@@ -10,27 +10,27 @@ import src.user.Customer;
 import src.user.Worker;
 
 public class TestCourier {
-	
-  /** 
+
+  /**
    * @param kebelekCafe
    */
   public static void testCourier(Restaurant kebelekCafe) {
     Courier courier1 = new Courier(
-      "Tolstoy",
-      50,
-      "tlsty",
-      "5652",
-      "+905055332577",	// phoneNumber
-      11    // experienceYear
+            "Tolstoy",
+            50,
+            "tlsty",
+            "5652",
+            "+905055332577",	// phoneNumber
+            11    // experienceYear
     );
 
     Courier courier2 = new Courier(
-      "Gogol",
-      45,
-      "mhmt1",
-      "4565",
-      "+905055932576",	// phoneNumber
-      17    // experienceYear
+            "Gogol",
+            45,
+            "mhmt1",
+            "4565",
+            "+905055932576",	// phoneNumber
+            17    // experienceYear
     );
 
     // kebelekCafe objesi gorulebiliyor mu
@@ -52,28 +52,22 @@ public class TestCourier {
     System.out.println("\n\n");
 
     System.out.println("TESTING ADD ORDER METHOD OF COURIER CLASS...");
-    courier1.addOrder(new Order(1, kebelekCafe.getTestCustomer(), foods, "dest1"));
+    courier1.addOrder(new Order(1, kebelekCafe.getTestCustomer(), foods, "RESTAURANT"));
     System.out.println("\n\n");
 
     System.out.println("TESTING DELIVER ORDER TO CUSTOMER METHOD OF COURIER CLASS...");
     courier1.deliverOrderToCustomer();
     System.out.println("\n\n");
 
-    System.out.println("TESTING FIND VALUE OF DISTRICT METHOD OF COURIER CLASS");
-// !!!    courier1.findValueOfDistrict();
-    System.out.println("\n\n");
 
     System.out.println("TESTING SHOW SHORTEST ROUTE METHOD OF COURIER CLASS");
-// !!!    courier1.showShortestRoute();
+    courier1.showShortestRoute("CAYIROVA");
     System.out.println("\n\n");
 
     System.out.println("TESTING COMPARE TO METHOD...");
     courier1.compareTo(courier2);
     System.out.println("\n\n");
 
-    System.out.println("TESTING GET SHORTEST ROUTE METHOD OF COURIER CLASS");
-// !!!    courier1.getShortestRoute();
-    System.out.println("\n\n");
 
     System.out.println("TESTING TO STRING METHOD OF COURIER CLASS...");
     System.out.println(courier1.toString());
