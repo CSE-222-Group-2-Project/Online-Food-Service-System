f4f1dec1621bd70f3510e0c6e0eff9fcee77c575
M.Sinan Pehlivanoglu <pehlivanoglumuhammedsinan@gmail.com>
2022-06-14 12:49:48 +0300

    Customer için  Comparator eklendi.

--- Driver.java
+++ Driver.java
@@ -1,10 +1,21 @@
 import src.auth.Authentication;
+import src.restaurant.Restaurant;
 import src.user.Chef;
+import src.user.Courier;
 import src.user.User;
+
+import java.util.ArrayList;
 
 public class Driver {
 
   public static void main(String[] args) {
-    User user = Authentication.login();
+
+
+    //User user = Authentication.login();
+    Restaurant restaurant = new Restaurant();
+    ArrayList<Integer> route = restaurant.getTestCourier().showShortestRoute("CUMHURIYET");
+    for (int parent: route) {
+      System.out.println(parent);
+    }
   }
 }
