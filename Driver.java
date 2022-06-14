8f023161f559e07c66b7ed7320778e8737489e63
serhhatsari <serhhatsari@gmail.com>
2022-06-14 13:41:08 +0300

    driver updated

--- Driver.java
+++ Driver.java
@@ -1,21 +1,17 @@
-import src.auth.Authentication;
-import src.restaurant.Restaurant;
-import src.user.Chef;
-import src.user.Courier;
-import src.user.User;
-import src.demo.DemoCustomer;
-
 import java.util.ArrayList;
+import src.demo.DemoCustomer;
+import src.restaurant.Restaurant;
 
 public class Driver {
 
   public static void main(String[] args) {
     Restaurant restaurant = new Restaurant();
-    ArrayList<Integer> route = restaurant.getTestCourier().showShortestRoute("CUMHURIYET");
-    for (int parent: route) {
+    ArrayList<Integer> route = restaurant
+      .getTestCourier()
+      .showShortestRoute("CUMHURIYET");
+    for (int parent : route) {
       System.out.println(parent);
-
-    testDemos();
+    }
   }
 
   public static void testUsers() {}
