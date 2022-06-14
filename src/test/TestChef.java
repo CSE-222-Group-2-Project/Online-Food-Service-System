efc330148b17b124105e02bc8e764bf7a79c2042
Asuman Sare ERGÜT <asumansaree@gmail.com>
2022-06-14 15:39:47 +0300

    chef and courier's test and demo's are added

--- src/test/TestChef.java
+++ src/test/TestChef.java
@@ -1,3 +1,68 @@
 package src.test;
 
-public class TestChef {}
+import src.restaurant.Food;
+import src.restaurant.Menu;
+import src.restaurant.Restaurant;
+import src.user.Chef;
+import src.user.Worker;
+
+public class TestChef {
+	public static void testChef() {
+    Chef chef1 = new Chef(
+      "Somer Chef",
+      50,
+      "smr1234",
+      "1335",
+      5,	// certificateNumber
+      15    // experienceYear
+    );
+
+    Chef chef2 = new Chef(
+      "Mehmet Chef",
+      45,
+      "mhmt1",
+      "4565",
+      6,	// certificateNumber
+      12    // experienceYear
+    );
+
+    // kebelekCafe objesi gorulebiliyor mu
+    CustomLinkedList<Food> foods = kebelekCafe.createRandomFoods();
+
+    System.out.println("TESTING CHEF METHODS...");
+    System.out.println("\n\n");
+
+    System.out.println("TESTING ADD ORDER METHOD OF CHEF CLASS...");
+    // !!! owner of the order should be change according to customer name
+    chef1.addOrder(new Order(1, "Buse", foods, "dest1"));
+    System.out.println("\n\n");
+
+    System.out.println("TESTING PREPARE ORDER METHOD OF CHEF CLASS...");
+    chef1.prepareOrder();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING GET CERTIFICATE NUMBER METHOD OF CHEF CLASS...");
+    chef1.getCertificateNumber();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING INCREMENT CERTIFICATE NUMBER METHOD OF CHEF CLASS...");
+    chef1.incrementCertificateNumber();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING CALCULATE STATUS METHOD OF CHEF CLASS");
+    chef1.calculateStatus();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING GET SIZE OF ORDERS METHOD OF CHEF CLASS...");
+    chef1.getSizeOfOrders();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING COMPARE TO METHOD...");
+    chef1.compareTo(chef2);
+    System.out.println("\n\n");
+
+    System.out.println("TESTING TO STRING METHOD OF CHEF CLASS...");
+    System.out.println(chef1.toString());
+  }
+}
+
--- src/test/TestCourier.java
+++ src/test/TestCourier.java
@@ -1,3 +1,75 @@
 package src.test;
 
-public class TestCourier {}
+import src.restaurant.Food;
+import src.restaurant.Menu;
+import src.restaurant.Restaurant;
+import src.user.Courier;
+import src.user.Worker;
+
+public class TestCourier {
+	public static void testCourier() {
+    Courier courier1 = new Courier(
+      "Tolstoy",
+      50,
+      "tlsty",
+      "5652",
+      +905055332577,	// phoneNumber
+      11    // experienceYear
+    );
+
+    Courier courier2 = new Courier(
+      "Gogol",
+      45,
+      "mhmt1",
+      "4565",
+      +905055932576,	// phoneNumber
+      17    // experienceYear
+    );
+
+    // kebelekCafe objesi gorulebiliyor mu
+    CustomLinkedList<Food> foods = kebelekCafe.createRandomFoods();
+
+    System.out.println("TESTING COURIER METHODS...");
+    System.out.println("\n\n");
+
+    System.out.println("TESTING SET PHONE NUMBER METHOD OF COURIER CLASS...");
+    courier1.setPhoneNumber("+905155942876");
+    System.out.println("\n\n");
+
+    System.out.println("TESTING GET PHONE NUMBER METHOD OF COURIER CLASS...");
+    courier1.getPhoneNumber();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING GET SIZE OF ORDERS METHOD OF COURIER CLASS...");
+    courier1.getSizeOfOrders();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING ADD ORDER METHOD OF COURIER CLASS...");
+    courier1.addOrder(new Order(1, "Buse", foods, "dest1"));
+    System.out.println("\n\n");
+
+    System.out.println("TESTING DELIVER ORDER TO CUSTOMER METHOD OF COURIER CLASS...");
+    courier1.deliverOrderToCustomer();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING FIND VALUE OF DISTRICT METHOD OF COURIER CLASS");
+// !!!    courier1.findValueOfDistrict();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING SHOW SHORTEST ROUTE METHOD OF COURIER CLASS");
+// !!!    courier1.showShortestRoute();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING COMPARE TO METHOD...");
+    courier1.compareTo(courier2);
+    System.out.println("\n\n");
+
+    System.out.println("TESTING GET SHORTEST ROUTE METHOD OF COURIER CLASS");
+// !!!    courier1.getShortestRoute();
+    System.out.println("\n\n");
+
+    System.out.println("TESTING TO STRING METHOD OF COURIER CLASS...");
+    System.out.println(courier1.toString());
+  }
+}
+
